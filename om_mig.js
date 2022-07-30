d3aa430f18e9d255b57641c83a9084c825a154c0
Yuliya Hagberg <yuliya.swe@gmail.com>
2022-07-30 21:46:57 +0200

    Scroll up button fixed

--- om_mig.js
+++ om_mig.js
@@ -1,35 +1,19 @@
-// $(document).ready(function() {
-//     $(window).scroll(function(){
-//         if(this.scrollY > 100) {
-//             $('.scroll-up').addClass("show");
-//         } else {
-//             $('.scroll-up').removeClass("show");
-//         }
-//     })
-// });
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-// // slide up
-// $('.scroll-up').click(function() {
-//     $('html').animate({scrollTop: 0});
-// });
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-const scrollBtn = document.querySelector(".scroll-up");
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-const refreshBtnVisibility = () => {
-    if (document.documentElement.scrollTop > 250) {
-        scrollBtn.style.display = "block";
-    } else {
-        scrollBtn.style.display = "none";
-    }
-};
-
-scrollBtn.addEventListener("click" , () => {
-    window.scroll({
-        top: 0,
-        behavior: "smooth"
-    })
-});
-
-document.addEventListener("scroll", (e) => {
-    refreshBtnVisibility();
-});
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
